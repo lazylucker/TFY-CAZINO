0fb2b053fdae15d0a5248e95c103c05a74644e70
lazylucker <clocz228@gmail.com>
2025-03-02 22:00:34 +0300

    Delete bot directory

--- bot/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher
-
-from .handlers import register_handlers
-
-
-def setup_bot(dp: Dispatcher):
-    """Функция для настройки бота и регистрации обработчиков."""
-    register_handlers(dp)
